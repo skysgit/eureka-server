186906f4162de4ec21c41f40fa3db7c2e5392fd9
skysgit <393740104@qq.com>
2020-02-23 22:52:20 +0800

    配置eureka-server

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # euruka-server
 euruka-server
+
+版本说明
+SC	      SB
+Hoxton	  	2.2.x
+Greenwich	2.1.x
+Finchley	2.0.x
+Edgware	 	1.5.x
+Dalston		1.5.x
+
--- src/main/java/com/cn/app/server/EurukaServerApp.java
+++ src/main/java/com/cn/app/server/EurukaServerApp.java
@@ -1,18 +1,11 @@
 package com.cn.app.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * 这里没有使用SpringBootApplication或SpringCloudApplication注解，会报错
- * 原因也很简单，我们的java源码目录下没有目录，我们手动加一个也就正常了，
- * 为了写点体会和这里没必要用到包和类，所以使用这种方式
- */
-@EnableConfigServer
-@SpringBootConfiguration
-@EnableAutoConfiguration
+@EnableEurekaServer
+@SpringBootApplication	
 public class EurukaServerApp {
 	  public static void main(String[] args) {
 	        SpringApplication.run(EurukaServerApp.class, args);
